3b2eabc51fef8be93944e5d2db8dc7f5a66c510c
jasonwynn10 <jasonwynn10@me.com>
2020-12-12 23:30:20 -0500

    Made packet code more consistent with the core

--- src/jasonwynn10/beacon/packet/InventoryTransactionPacketV2.php
+++ src/jasonwynn10/beacon/packet/InventoryTransactionPacketV2.php
@@ -21,7 +21,7 @@ class InventoryTransactionPacketV2 extends InventoryTransactionPacket {
 		$this->hasItemStackIds = $this->getBool();
 
 		for($i = 0, $count = $this->getUnsignedVarInt(); $i < $count; ++$i){
-			$this->actions[] = $action = (new CustomInventoryAction())->read($this, $this->hasItemStackIds);
+			$this->actions[] = (new CustomInventoryAction())->read($this, $this->hasItemStackIds);
 		}
 
 		$this->trData = new \stdClass();
